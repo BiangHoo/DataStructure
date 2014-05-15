c8a7a1a6a7e1342e34467bc7be25e79f6f443339
BiangHoo <BiangHoo@gmail.com>
2014-05-15 10:57:59 +0800

    for test
    add function sort(),

--- linklist.cpp
+++ linklist.cpp
@@ -105,14 +105,62 @@ class List{
 		if(head==NULL||k<0||k>this->length()-1){
 			return -1;
 		}
-		if(k=0){
+		if(k==0){
 			tmp=head;
 			node* pNew = new Node;
 			pNew->value=val;
 			pNew->pNext=NULL;
-			head=pNew;
 			pNew->pNext=tmp;
+			head=pNew;
+		}else{
+			tmp = head;
+			int i=0;
+			while(i<k-1){
+				tmp=tmp->pNext;
+				i++;
+			}
+			p1=tmp->pNext;
+			node *pNew =new Node;
+			pNew->value=val;
+			pNew->pNext=NULL;
+			tmp->pNext=pNew;
+			pNew->pNext=p1;
 		}
+		return 0;
+	}
+	int getHeadVal(){
+		return head->value;
+	
+	}
+	int findVal(int val){
+		int i=0;
+		node* tmp =head;
+		while(tmp!=NULL){
+			if(tmp->value == val){
+				return i;
+			}
+			tmp=tmp->pNext;
+			i++;
+		}
+		return -1;
+	}
+	void sort(){
+		int n= this->length();
+		node* p1;
+		node* p2;
+		for(int i=0;i<n-1;i++){
+			p1=head;
+			for(int j=0;j<n-1-i;j++){
+				p2=p1->pNext;
+				if(p1->value>p2->value){
+					int num = p1->value;  
+                    			p1->value = p2->value;  
+                    			p2->value = num;
+				}
+				p1 =p1->pNext;
+			}
+		}
+
 	}
 };
 int main(){
@@ -123,6 +171,7 @@ int main(){
 	cout<<"------"<<endl;
 	list.print();
 	cout<<list.length()<<endl;
+	cout<<"head: "<<list.getHeadVal()<<endl;
 	cout<<"------"<<endl;
 	list.delAt(2);
 	list.print();
@@ -131,9 +180,23 @@ int main(){
 	list.delAt(0);
 	list.print();
 	cout<<list.length()<<endl;
+	cout<<"head :"<<list.getHeadVal()<<endl;
 	cout<<"------"<<endl;
 	list.delAt(list.length()-1);
 	list.print();
+	cout<<"head: "<<list.getHeadVal()<<endl;
 	cout<<list.length()<<endl;
+	cout<<"---------------------"<<endl;
+	list.insertAt(5,0);
+	list.print();
+	cout<<"head: "<<list.getHeadVal()<<endl;
+	list.insertAt(7,1);
+	list.print();
+	cout<<"head :"<<list.getHeadVal()<<endl;
+	cout<<list.length()<<endl;
+	cout<<list.findVal(7)<<endl;
+	cout<<"......................"<<endl;
+	list.sort();
+	list.print();
 	return 0;
 }
